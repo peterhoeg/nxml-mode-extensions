874fcd0d424354abb5fab393668b928f313173f7
Peter Hoeg <peter@hoeg.com>
2020-10-14 10:22:09 +0800

    add support for pretty printing

--- nxml-mode-extensions.el
+++ nxml-mode-extensions.el
@@ -1,6 +1,6 @@
 ;;; nxml-mode-extensions.el --- nxml-mode extensions  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019  Peter Hoeg
+;; Copyright (C) 2019-2020  Peter Hoeg
 
 ;; Author: Peter Hoeg <peter@hoeg.com>
 ;; Keywords: languages,
@@ -83,6 +83,39 @@
        "<!--"
        sgml-skip-tag-forward
        nil))))
+
+;; http://stackoverflow.com/a/5198243/48082
+;;;###autoload
+(defun xml-pretty-print-region (begin end)
+  "Pretty format XML markup in region."
+  (interactive "r")
+  (save-excursion
+    ;; (nxml-mode)
+    ;; split <foo><bar> or </foo><bar>, but not <foo></foo>
+    (goto-char begin)
+    (while (search-forward-regexp ">[ \t]*<[^/]" end t)
+      (backward-char 2) (insert "\n") (cl-incf end))
+    ;; split <foo/></foo> and </foo></foo>
+    (goto-char begin)
+    (while (search-forward-regexp "<.*?/.*?>[ \t]*<" end t)
+      (backward-char) (insert "\n") (cl-incf end))
+    ;; put xml namespace decls on newline
+    (goto-char begin)
+    (while (search-forward-regexp "\\(<\\([a-zA-Z][-:A-Za-z0-9]*\\)\\|['\"]\\) \\(xmlns[=:]\\)" end t)
+      (goto-char (match-end 0))
+      (backward-char 6) (insert "\n") (cl-incf end))
+    (indent-region begin end nil)
+    ;; since we are changing things anyway, we might as well clean up whitespace
+    (delete-trailing-whitespace begin end)
+    (normal-mode)))
+
+;;;###autoload
+(defun xml-pretty-print-region-or-buffer ()
+  "Pretty format XML in region or buffer."
+  (interactive)
+  (if (region-active-p)
+      (xml-pretty-print-region (region-beginning) (region-end))
+    (xml-pretty-print-region (point-min) (point-max))))
 
 (provide 'nxml-mode-extensions)
 ;;; nxml-mode-extensions.el ends here
